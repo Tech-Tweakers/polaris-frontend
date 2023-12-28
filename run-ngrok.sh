ffb5a66b99a79580d0dbd52a7e24dcd19b79fbea
Andre Torres <andreh.torres@hotmail.com>
2023-12-28 16:13:40 -0300

    fix: README.md

--- run-ngrok.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ngrok tunnel --label edge=edghts_2WdYVJdfIYZvkUcINgYFydYIiIN https://192.168.2.37:443
--- run-server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/atorres/Dev/JS/ztech-front-end-reactjs/
-npm start
